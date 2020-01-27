5d5f193af249393e73318b6af7c40aa7fef64fa9
Oleksii <matalexnin@gmail.com>
2020-01-28 01:28:47 +0200

    Updates

---
+++ precache-manifest.78d1921fb316b03e691693d430b8710b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8070b1b671fe6fe68d519bb964f8d75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5caf7850ab03fdb6bb5",
+    "url": "/static/css/main.80c59804.chunk.css"
+  },
+  {
+    "revision": "4eebcf32df0faf575af9",
+    "url": "/static/js/2.c6d1a0e6.chunk.js"
+  },
+  {
+    "revision": "b5caf7850ab03fdb6bb5",
+    "url": "/static/js/main.a0a19019.chunk.js"
+  },
+  {
+    "revision": "5d290778acb6861137c6",
+    "url": "/static/js/runtime-main.f47aeb5e.js"
+  }
+]);
--- src/functions.js
+++
@@ -1,5 +0,0 @@
-export const getTomorrow = () => {
-    const d = new Date();
-    const now_utc =  Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate() + 2,8, 0, 0);
-    return new Date(now_utc);
-};
